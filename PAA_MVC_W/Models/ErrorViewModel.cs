ad1db47cbc1b94fae52928e752c9a7b832ba67f4
oadamian <trackzc_68@hotmail.com>
2024-08-22 10:01:59 -0600

    first commit

---
+++ PAA_MVC_W/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PAA_MVC_W.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
